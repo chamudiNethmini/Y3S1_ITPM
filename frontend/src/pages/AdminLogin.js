2319f4b2172f008dd14316318e8fcbf254c655e8
gayanthawanni <gayanthawannisekara@gmail.com>
2026-02-16 18:40:53 +0530

    Add frontend project

---
+++ frontend/src/pages/AdminLogin.js
@@ -0,0 +1,12 @@
+function AdminLogin() {
+  return (
+    <div className="page">
+      <h2>Admin Login</h2>
+      <input type="email" placeholder="Enter Email" />
+      <input type="password" placeholder="Enter Password" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default AdminLogin;
---
+++ frontend/src/pages/LecturerLogin.js
@@ -0,0 +1,12 @@
+function LecturerLogin() {
+  return (
+    <div className="page">
+      <h2>Lecturer Login</h2>
+      <input type="email" placeholder="Enter Email" />
+      <input type="password" placeholder="Enter Password" />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default LecturerLogin;
